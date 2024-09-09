924da2a5a2d116dbe32c58b3b2b6619f84f2b44b
pinyotae <pinyotae@gmail.com>
2024-09-10 01:05:58 +0700

    Threading done, circular buffer implemented. Time is reported in file. Next, accept arguments with JSON.

--- FramePacer/FramePacer.cpp
+++ FramePacer/FramePacer.cpp
@@ -7,6 +7,7 @@
 // TODO: Save videos at interval
 
 #include <iostream>
+#include <fstream>
 #include <opencv2/opencv.hpp>
 #include <memory>
 #include <omp.h>
@@ -58,19 +59,23 @@ string series_name = "demo_";
 string img_out_folder = "C:/TestingGround/VideoCapture/Images";
 string imgFileFormatStr;
 string video_out_folder = "C:/TestingGround/VideoCapture/Videos";
-int ioBufferSize = 3000;
-std::atomic<int> bufferEndIndex(-1);
-std::atomic<int> bufferStartIndex(-1);
-int maxBufferIndexLead = 0;
+string timeStampReportFileName = "time_stamp_report.tab";
+string timeDeviationReportFileName = "time_deviation_report.tab";
+int ioBufferSize = 30;
+std::atomic<int> bufferEndIndex(0);
+std::atomic<int> bufferStartIndex(0);
+int framesNotWritten = 0;
+int framesLeftToCapture = 0;
 std::mutex writeMutex;
+int timeBetweenFramesMSec;
 cv::Mat saveBuffer;
+int numFrames;
 
 std::atomic<int> grabCount(0);
 std::atomic<int> writeCount(0);
 
 
-int main()
-{
+int main() {
     std::cout << "Start of Video Capturing\n";
 	const int camID = 0;
 	const int frameHeight = 480;
@@ -79,14 +84,17 @@ int main()
 	cout << "Target frame rate = " << targetFPS << "\n";
 	shrptr_VideoCapture cap = initVideoCapture(camID, frameHeight, frameWidth, targetFPS);
 	
-	const int numSeconds = 3;
-	int numFrames = (int)(targetFPS * numSeconds);
+	const int numSeconds = 5;
+	numFrames = (int)(targetFPS * numSeconds);
+	cout << "Number of frames = " << numFrames << "\n";
+	framesLeftToCapture = numFrames;
 	setImgFileNameFormatString(numFrames);
 	cap->set(cv::CAP_PROP_FPS, targetFPS);
 	double actualFPS = cap->get(cv::CAP_PROP_FPS);
 	
 	const double timeBetweenFrames = 1.0 / targetFPS;
-	cout << "Time between frames = " << (int)(timeBetweenFrames * 1000) << " msec\n";
+	timeBetweenFramesMSec = (int)(timeBetweenFrames * 1000);
+	cout << "Time between frames = " << timeBetweenFramesMSec << " msec\n";
 
 	captureToMemorySpace(cap, timeBetweenFrames, numFrames, (int) targetFPS);
 }
@@ -150,10 +158,13 @@ shrptr_VideoCapture initVideoCapture(const int camID, const int frameHeight,
 }
 
 
-/// Removing some unnecessary steps and let the wait time end a little bit earlier.
+/// To pace frame arrival, we compute the time our application should wait before
+///   issuing the next frame grab command.
+/// The wait time end a little bit earlier by marginTime.
 /// The early end of wait time is to compensate the incoming work before the actual
-///   video capturing.
-int waitForNextTime2(int nextFrameID, double timeBetweenFrame, double time0) {
+///   video capturing. We should calibrate the marginTime to make frame pacing close
+///   to the ideal as much as possible.
+int waitForNextGrab(int nextFrameID, double timeBetweenFrame, double time0) {
 	double currTime = omp_get_wtime();
 	double elapsedTime = currTime - time0;
 	double nextTime = nextFrameID * timeBetweenFrame;
@@ -161,7 +172,7 @@ int waitForNextTime2(int nextFrameID, double timeBetweenFrame, double time0) {
 	// Capture thread will awake before the expected capture time by the amount of
 	//   margin time. For example, if marginTime is 0.002, the thread will awake 2 ms
 	//   before the expected capture time.
-	const double marginTime = 0.002;  
+	const double marginTime = 0.003;  
 	int waitTime = -1;
 	if (elapsedTime < nextTime - marginTime) { // Need to wait until the next time
 		waitTime = (int)((nextTime - elapsedTime - marginTime) * 1000);
@@ -173,10 +184,9 @@ int waitForNextTime2(int nextFrameID, double timeBetweenFrame, double time0) {
 
 
 void prepareEmptyFrames(std::vector<cv::Mat>& frames, const int height,
-	const int width, const int numFrames)
-{
+		const int width) {
 	saveBuffer = cv::Mat(height, width, CV_8UC3);
-	for (int frameID = 0; frameID < numFrames; ++frameID) {
+	for (int frameID = 0; frameID < ioBufferSize; ++frameID) {
 		frames.at(frameID) = cv::Mat(height, width, CV_8UC3);
 	}
 }
@@ -186,8 +196,7 @@ void prepareEmptyFrames(std::vector<cv::Mat>& frames, const int height,
 ///   based on the reference time0.
 /// @return elapsed time from the beginning of processing (time0).
 double pushFrameToMat(shrptr_VideoCapture cap, double time0, vector<Mat>& frames, 
-	int frameID)
-{
+		int frameID) {
 	cap->retrieve(frames.at(frameID));
 
 	double currTime = omp_get_wtime();
@@ -197,14 +206,16 @@ double pushFrameToMat(shrptr_VideoCapture cap, double time0, vector<Mat>& frames
 }
 
 
-void writeFrameToImageFile(int frameID, vector<Mat>* frames, std::mutex* mutexVid) {
+void writeFrameToImageFile(int frameID, vector<Mat>* frames) {
 	//std::lock_guard<std::mutex> lock(*mutexVid);
-	string imgPath = fmt::format(imgFileFormatStr, frameID);
+	string imgPath = fmt::format(imgFileFormatStr, img_out_folder, series_name, frameID);
 
-	mutexVid->lock();
-	saveBuffer = frames->at(bufferStartIndex);
-	bufferStartIndex = (bufferStartIndex + 1) % ioBufferSize;
-	mutexVid->unlock();
+	writeMutex.lock(); {
+		saveBuffer = frames->at(bufferStartIndex);
+		bufferStartIndex = (bufferStartIndex + 1) % ioBufferSize;
+		framesNotWritten -= 1;
+	}
+	writeMutex.unlock();
 
 	imwrite(imgPath, saveBuffer);
 }
@@ -213,17 +224,17 @@ void writeFrameToImageFile(int frameID, vector<Mat>* frames, std::mutex* mutexVi
 double pushFrameToMatCircularBuffer(shrptr_VideoCapture cap, double time0,
 		vector<Mat>& frames, int frameID) 
 {
-	if ((bufferEndIndex + 1) % ioBufferSize == bufferStartIndex) {
-		printf("Error: I/O buffer is full.\n");
-		exit(0);
+	writeMutex.lock(); {
+		if ((bufferEndIndex + 1) % ioBufferSize == bufferStartIndex) {
+			printf("Error: I/O buffer is full.\n");
+			exit(0);
+		}
+		bufferEndIndex = (bufferEndIndex + 1) % ioBufferSize;
+		cap->retrieve(frames.at(bufferEndIndex));
+		framesNotWritten += 1;
+		framesLeftToCapture -= 1;
 	}
-	bufferEndIndex = (bufferEndIndex + 1) % ioBufferSize;
-
-	cap->retrieve(frames.at(bufferEndIndex));
-
-	// Create threads for writing a frame
-	std::thread writeThread(writeFrameToImageFile, frameID, &frames, &writeMutex);
-	//writeThread.join(); // Synchronize within the loop
+	writeMutex.unlock();
 
 	double currTime = omp_get_wtime();
 	double elapsedTime = currTime - time0;	// record how much time passed (milli-second)
@@ -232,10 +243,33 @@ double pushFrameToMatCircularBuffer(shrptr_VideoCapture cap, double time0,
 }
 
 
-void reportTimeStamps(vector<double>& grabTime, vector<double>& retrieveTime) {
-	for (int i = 0; i < grabTime.size(); ++i) {
-		printf("%d\t%f\t%f\n", i + 1, grabTime.at(i), retrieveTime.at(i));
+void saveFramesThd(vector<Mat>* frames) {
+	int frameID = 0;
+	while (framesLeftToCapture > 0 || framesNotWritten > 0) {
+		if (framesNotWritten == 0) {  // Wait for a grabber to get another frame.
+			std::this_thread::sleep_for(std::chrono::milliseconds(timeBetweenFramesMSec));
+			continue;
+		}
+		writeFrameToImageFile(frameID, frames);
+		frameID += 1;
 	}
+}
+
+
+// Report the time stamp of each frame to a file.
+void reportTimeStamps(vector<double>& grabTime, vector<double>& retrieveTime) {
+	string timeStampPath = img_out_folder + "/" + timeStampReportFileName;
+	cout << "Saving the time stamp of each frame to " << timeStampPath << "\n";
+	ofstream reportFile(timeStampPath);
+
+	reportFile << "FrameID\tGrabTime(s)\tRetrievalTime(s)\n";
+	for (int i = 0; i < grabTime.size(); ++i) {
+		reportFile << fmt::format("{}\t{}\t{}\n", i + 1, grabTime.at(i), retrieveTime.at(i));
+		if (i % 100 == 0)  // Print a dot for each 100 lines saved
+			printf(".");
+	}
+	reportFile.close();
+	cout << "\nSaving time stamps DONE" << endl;
 }
 
 
@@ -256,13 +290,14 @@ void setImgFileNameFormatString(const int numFrames) {
 
 
 void exportAllImages(vector<Mat>& frames) {
-	const int nFrames = (int) frames.size();
-	printf("Saving all %d images.\n", nFrames);	
-
-	for (int i = 0; i < nFrames; ++i) {
+	printf("Saving all %d images.\n", numFrames);	
+	for (int i = 0; i < numFrames; ++i) {
 		string img_path = fmt::format(imgFileFormatStr, img_out_folder, series_name, i);
 		imwrite(img_path, frames.at(i));
+		if (i % 100 == 0)  // Print a dot for each 100 images saved.
+			printf(".");
 	}
+	cout << "\nSaving all images DONE\n";
 }
 
 
@@ -292,21 +327,54 @@ void warmUpGrabbingAndRetrieving(shrptr_VideoCapture cap, Mat dummyFrame) {
 
 void reportGrabTimeAndDeviation(const int numFrames, const double timeBetweenFrames,
 		vector<double>& grabTimeStamps, vector<int>& waitTimes) {
+	string reportPath = img_out_folder + "/" + timeDeviationReportFileName;
+	cout << "Saving deviation of frame arrival time to " << reportPath << endl;
+	ofstream reportFile(reportPath);
+	reportFile << "FrameID\t" << "FrameTime(ms)\t" << "WaitTime(ms)\t" << "ArrivalTimeDeviation(ms)\n";
+
 	double previousTimeStamp = 0;
 	int timeDiffSum = 0;
 	for (int frameID = 0; frameID < numFrames; ++frameID) {
 		double grabTime = grabTimeStamps.at(frameID);
 		double frameStartTime = timeBetweenFrames * frameID;
 		double expectedTime = (timeBetweenFrames * (frameID + 1));
-		int timeDiff = (int)((grabTime - expectedTime) * 1000);
-		printf("frame %3d at %4.4f, wait time = %2d, deviation of arrival time = %3d msec\n",
-			frameID + 1, grabTime - frameStartTime,
-			waitTimes.at(frameID), timeDiff);
+		double timeDiff = (grabTime - expectedTime) * 1000;
+		reportFile << fmt::format("{:3d}\t{:5.2f}\t{:2d}\t{:.2f}\n",
+			frameID + 1, (grabTime - frameStartTime) * 1000, waitTimes.at(frameID), 
+			timeDiff);
 		timeDiffSum += abs(timeDiff);
 		previousTimeStamp = grabTimeStamps.at(frameID);
+		if (frameID % 100 == 0)  // Print a dot for every 100 lines saved.
+			cout << ".";
 	}
-	printf("Total deviation time = %d ms, average deviation time = %.3f ms\n",
+	reportFile << fmt::format("\nTotal absolute deviation time = {} ms, average absolute deviation time = {:.3f} ms\n",
 		timeDiffSum, timeDiffSum / (double)numFrames);
+	reportFile.close();
+	cout << "\nSaving time deviation DONE" << endl;
+	printf("Total absolute deviation time = %d ms, average absolute deviation time = %.3f ms\n",
+		timeDiffSum, timeDiffSum / (double)numFrames);
+}
+
+
+void grabPushWaitThdLoop(shrptr_VideoCapture cap, vector<Mat>* frames, 
+		const int numFrames, const double timeBetweenFrames, 
+		vector<double>* grabTimeStamps, vector<double>* retrieveTimeStamps, 
+		vector<int>* waitTimes)
+{
+	double time0 = omp_get_wtime();
+	for (int frameID = 0; frameID < numFrames; ++frameID) {
+		// Video frame is captured when grab is called. So, we compute the wait time
+		//   right before we call grab.	For example, at 30 fps, the first frame should be captured
+		//   at about t = 0.0333 second.
+		int waitTime = waitForNextGrab(frameID + 1, timeBetweenFrames, time0);
+		const double grabTimeStamp = omp_get_wtime() - time0;
+		cap->grab();  // Video frame is stored in a buffer, waiting for retrieval to RAM.
+		grabTimeStamps->push_back(grabTimeStamp);
+		waitTimes->push_back(waitTime);
+		double t = pushFrameToMatCircularBuffer(cap, time0, *frames, frameID);
+		retrieveTimeStamps->push_back(t);
+	}
+	cout << "Frame grapping completed.\n";
 }
 
 
@@ -319,7 +387,7 @@ void grabPushWaitLoop(shrptr_VideoCapture cap, vector<Mat>& frames, const int nu
 		// Video frame is captured when grab is called. So, we compute the wait time
 		//   right before we call grab.	For example, at 30 fps, the first frame should be captured
 		//   at about t = 0.0333 second.
-		int waitTime = waitForNextTime2(frameID + 1, timeBetweenFrames, time0);
+		int waitTime = waitForNextGrab(frameID + 1, timeBetweenFrames, time0);
 		const double grabTimeStamp = omp_get_wtime() - time0;
 		cap->grab();  // Video frame is stored in a buffer, waiting for retrieval to RAM.
 		grabTimeStamps.push_back(grabTimeStamp);
@@ -334,21 +402,35 @@ void grabPushWaitLoop(shrptr_VideoCapture cap, vector<Mat>& frames, const int nu
 void captureToMemorySpace(shrptr_VideoCapture cap, const double timeBetweenFrames,
 	const int numFrames, const int framesPerSec)
 {
-	vector<Mat> frames(numFrames);
+	vector<Mat> frames(ioBufferSize);  // Use parameter numFrames if all to be stored.
 	const int frameHeight = (int)cap->get(cv::CAP_PROP_FRAME_HEIGHT);
 	const int frameWidth = (int)cap->get(cv::CAP_PROP_FRAME_WIDTH);
-	prepareEmptyFrames(frames, frameHeight, frameWidth, numFrames);
+	prepareEmptyFrames(frames, frameHeight, frameWidth);
 	warmUpGrabbingAndRetrieving(cap, frames.at(0));
 	
 	vector<double> grabTimeStamps;
 	vector<double> retrieveTimeStamps;
 	vector<int> waitTimes;
-	grabPushWaitLoop(cap, frames, numFrames, timeBetweenFrames, grabTimeStamps,
-		retrieveTimeStamps, waitTimes);
+	
+	// Start a thread for frame grabbing
+	std::thread grabThread(grabPushWaitThdLoop, cap, &frames, numFrames, 
+		timeBetweenFrames, &grabTimeStamps, &retrieveTimeStamps, &waitTimes);
+
+	// Start a thread for saving video frames if I/O buffer cannot contain all frames
+	if (numFrames > ioBufferSize) {
+		std::thread frameSavingThread(saveFramesThd, &frames);
+		frameSavingThread.join();  // Synchronize within the loop
+	}	
+
+	grabThread.join();
 
 	reportTimeStamps(grabTimeStamps, retrieveTimeStamps);
 
-	exportAllImages(frames);
+	// If the buffer can hold the entire set of grabbed frames, we will write the frames
+	//   when all frames are available in the buffer.
+	if (numFrames <= ioBufferSize) {
+		exportAllImages(frames);
+	}
 	//exportVideo(frames, numFrames, framesPerSec);
 
 	reportGrabTimeAndDeviation(numFrames, timeBetweenFrames, grabTimeStamps, waitTimes);
